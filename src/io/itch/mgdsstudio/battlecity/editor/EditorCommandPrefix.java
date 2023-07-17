a99edf1dc807f9024abaf8b9cae0d8c8382209a1
Alexander Gorodilov <112826961+MGDSStudio@users.noreply.github.com>
2023-07-17 08:36:26 +0300

    Update EditorCommandPrefix.java

--- src/io/itch/mgdsstudio/battlecity/editor/EditorCommandPrefix.java
+++ src/io/itch/mgdsstudio/battlecity/editor/EditorCommandPrefix.java
@@ -1,6 +1,8 @@
 package io.itch.mgdsstudio.battlecity.editor;
 
 public enum EditorCommandPrefix {
+    BACK_PRESSED,
+    BUTTON_PRESSED,
     WORLD_SCROLLING,
     WORLD_SCROLLING_STARTED,
     WORLD_SCROLLING_ENDED;
