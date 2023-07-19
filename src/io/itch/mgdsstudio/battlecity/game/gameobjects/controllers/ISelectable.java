e777d5110b70579297654ad0d3def3d8bfde3946
Alexander Gorodilov <112826961+MGDSStudio@users.noreply.github.com>
2023-07-19 08:13:06 +0300

    Create ISelectable.java

---
+++ src/io/itch/mgdsstudio/battlecity/game/gameobjects/controllers/ISelectable.java
@@ -0,0 +1,10 @@
+package io.itch.mgdsstudio.battlecity.game.gameobjects.controllers;
+
+
+public interface ISelectable{
+
+    void select();
+    void clearSelection();
+    boolean isSelected();
+    
+}
