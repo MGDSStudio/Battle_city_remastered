41450c2a24f304915d6067c7cb1485002042d906
Alexander Gorodilov <112826961+MGDSStudio@users.noreply.github.com>
2023-07-29 17:37:23 +0300

    Create ByTimerActivatingController.java

---
+++ src/io/itch/mgdsstudio/battlecity/game/gameobjects/controllers/ByTimerActivatingController.java
@@ -0,0 +1,25 @@
+package io.itch.mgdsstudio.battlecity.game.gameobjects.controllers;
+
+public abstract class ByTimerActivatingController extends ObjectAppearingController{
+
+    private final int activatingTime;
+
+    ByTimerActivatingController(Entity entity, ArrayList <Integer> values){
+          super(entity, values);
+          activatingTime = values.get(1);
+    }
+
+    public void update (){
+        if (!activated){
+              if (engine.getEngine().millis()>=activatingTime){
+                notify();
+                activated = true;
+              }
+        }
+
+    }
+    
+
+    
+      
+}
