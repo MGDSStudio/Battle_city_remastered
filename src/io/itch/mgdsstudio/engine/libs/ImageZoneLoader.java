0cb2fa80af524716f94d33f36ba34555e675ae33
Alexander Gorodilov <112826961+MGDSStudio@users.noreply.github.com>
2023-08-01 08:05:35 +0300

    Create ImageZoneLoader.java

---
+++ src/io/itch/mgdsstudio/engine/libs/ImageZoneLoader.java
@@ -0,0 +1,19 @@
+package io.itch.mgdsstudio.engine.libs;
+
+public class ImageZoneLoader{
+  private String name;
+  private final static String LEFT = "LEFT";
+  private final static String UP = "UP";
+  private final static String RIGHT = "RIGHT";
+  private final static String DOWN = "DOWN";
+  private final ImageZoneSimpleData data;
+  
+  public ImageZoneLoader(String path){
+    
+  }
+
+  public ImageZoneSimpleData getData(){
+    return data;
+  }
+}
+  
